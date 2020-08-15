0f2350ba696817a41a3f56b18cfafad802bc1ab2
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-15 21:54:20 +0200

    primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Web Scraping
+
+from selenium import webdriver
+from selenium.common.exceptions import *
+
+# Data manipulation
+
+import pandas as pd 
+
+# Visualization
+
+import matplotlib.pyplot as plt
+import seaborn as sns
