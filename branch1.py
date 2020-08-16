1331035f5a669f67dec56c5a33a8beda70765871
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-16 12:52:11 +0200

    branch1 100% done

--- branch1.py
+++ branch1.py
@@ -5,5 +5,9 @@ url = 'https://www.backmarket.es/iphone-x-64-gb-gris-espacial-libre-segunda-mano
 response = requests.get(url)
 content = BeautifulSoup(response.content, "html.parser")
 
-precio = content.find('div', attrs={"class":"_1OTebAgHAcWddAP7YhPlZr"})
-print(precio.text)
+for precio in content.find('div', attrs={"class":"_1OTebAgHAcWddAP7YhPlZr"}):
+    precioObject= {
+        "Estado": precio.find('div', attrs={"class":"_3tVEyw2WXK4Ne75JtF4vL4"}).text.strip('\n\xa0€'),
+        "Precio": precio.find('div', attrs={"class":"_1KnTWVHWxp0RXYEjGmT44-"}).text.strip('\n\xa0€')
+    }
+    print(precioObject)
