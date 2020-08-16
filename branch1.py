2173144e2958a17aafe5b7e796f725210bd6c82b
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-16 13:03:19 +0200

    precios.csv creado

--- branch1.py
+++ branch1.py
@@ -6,8 +6,8 @@ response = requests.get(url)
 content = BeautifulSoup(response.content, "html.parser")
 
 for precio in content.find('div', attrs={"class":"_1OTebAgHAcWddAP7YhPlZr"}):
-    precioObject= {
+    precioXnegro= {
         "Estado": precio.find('div', attrs={"class":"_3tVEyw2WXK4Ne75JtF4vL4"}).text.strip('\n\xa0€'),
         "Precio": precio.find('div', attrs={"class":"_1KnTWVHWxp0RXYEjGmT44-"}).text.strip('\n\xa0€')
     }
-    print(precioObject)
+    print(precioXnegro)
