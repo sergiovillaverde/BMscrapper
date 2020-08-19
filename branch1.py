0fbc3bf2762eb5b715de8ff6fd9b729198bf6359
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-19 20:56:58 +0200

    trying

--- branch1.py
+++ branch1.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
-import requests
+from datetime import date
+import requests, csv
+import pandas as pd 
+
+today = date.today()
 
 url = 'https://www.backmarket.es/iphone-x-64-gb-gris-espacial-libre-segunda-mano/36833.html#?l=0'
 response = requests.get(url)
 content = BeautifulSoup(response.content, "html.parser")
 
 for precio in content.find('div', attrs={"class":"_1OTebAgHAcWddAP7YhPlZr"}):
-    precioXnegro= {
+    precioXnegro = {
         "Estado": precio.find('div', attrs={"class":"_3tVEyw2WXK4Ne75JtF4vL4"}).text.strip('\n\xa0€'),
         "Precio": precio.find('div', attrs={"class":"_1KnTWVHWxp0RXYEjGmT44-"}).text.strip('\n\xa0€')
     }
+    
+    with open('precios.csv', 'w') as f:
+        for key in precioXnegro.keys():
+            f.write("%s,%s\n"%(key,precioXnegro[key]))
+        
     print(precioXnegro)
