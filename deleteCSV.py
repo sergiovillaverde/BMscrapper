47e88722e18cac6ffc09198245120f19cc89359a
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-23 18:02:42 +0200

    delete csv working

--- deleteCSV.py
+++ deleteCSV.py
@@ -1,30 +1,8 @@
-import os, glob
+import os, glob, shutil
 from time import sleep
 import pyinputplus as pyip 
 
 CSVfiles = r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\CSV"
+os.chdir(CSVfiles)
 
-# Evitar que el programa se cierre al terminar
-while True:
-    borrar = pyip.inputChoice(['si', 'no'], prompt='¿Quieres eliminar los archivos CSV?\n')
-    if borrar == 'si':
-        try:
-            os.close(CSVfiles)
-            files = glob.glob(CSVfiles)
-            for f in files:
-                os.remove(f)
-            print('Archivos CSV eliminados.')
-            sleep(3)
-        except:
-            print('No se han podido eliminar los archivos CSV.')
-            
-        try:
-            os.mkdir(CSVfiles)
-        except:
-            print('No se ha podido crear de nuevo la carpeta CSV.')
-            
-        break
-    
-    elif borrar == 'no':
-        print('El programa se va a cerrar.')
-        break
+shutil.rmtree(CSVfiles)
