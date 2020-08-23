90441a8feaff04b45ee6d84418401b15784d40cf
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-23 11:26:39 +0200

    Improve experience

--- main.py
+++ main.py
@@ -18,32 +18,50 @@ from Models.iphoneXSdorado import iphoneXSdorado
 try:
     print('El programa se ha iniciado.')
     sleep(2)
-    print('Recopilando precios de iPhone X')
+    print('Recopilando precios de iPhone X.')
+    sleep(1)
+    print('iPhone X negro:')
     iphoneXnegro()
+    sleep(1)
+    print('iPhone X plata:')
     iphoneXplata()
+    sleep(1)
 
     print('Recopilando precios de iPhone XR.')
+    sleep(1)
+    print('iPhone XR negro:')
     iphoneXRnegro()
+    sleep(1)
+    print('iPhone XR rojo:')
     iphoneXRrojo()
+    sleep(1)
+    print('iPhone XR azul:')
     iphoneXRazul()
+    sleep(1)
+    print('iPhone XR coral:')
     iphoneXRcoral()
+    sleep(1)
+    print('iPhone XR amarillo:')
     iphoneXRamarillo()
+    sleep(1)
+    print('iPhone XR blanco:')
     iphoneXRblanco()
+    sleep(1)
 
     print('Recopilando precios de iPhone XS.')
+    sleep(1)
+    print('iPhone XS negro:')
     iphoneXSnegro()
+    sleep(1)
+    print('iPhone XS plata:')
     iphoneXSplata()
+    sleep(1)
+    print('iPhone XS dorado:')
     iphoneXSdorado()
+    sleep(1)
 
     print('Los archivos CSV están listos, el programa ha terminado.')
-    sleep(5)
-    
-    '''
-    os.chdir(r'C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper')
-    from combineCSV import combineCSV
-    combineCSV()
-    '''
-    
+    sleep(5)    
     
 except:
     print('Ha surgido un error, el programa se ha detenido.')
