0c8f0d9da3b187cb7e3071bd902014c167e5c457
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-24 15:52:20 +0200

    minnor changes

--- deleteCSV.py
+++ deleteCSV.py
@@ -4,16 +4,21 @@ import pyinputplus as pyip
 
 def deleteCSV():
     CSVfiles = r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\CSV"
-    os.chdir(CSVfiles)
+    #os.chdir(CSVfiles)
 
     borrar = pyip.inputChoice(['si','no'], prompt='¿Quieres eliminar los archivos CSV?\n')
     while True:
         if borrar == 'si':
-            shutil.rmtree(CSVfiles)
-            sleep(1)
-            print('Los archivos CSV han sido eliminados.')
-            sleep(5)
-            break
+            try:
+                shutil.rmtree(CSVfiles)
+                sleep(1)
+                print('Los archivos CSV han sido eliminados.')
+                sleep(5)
+                break
+            except Exception as err:
+                print('Ha ocurrido un error: '+ str(err))
+                sleep(10)
+                break
                 
         elif borrar == 'no':
             break
