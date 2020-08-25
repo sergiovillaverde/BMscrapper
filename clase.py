e52e08943ea5c123b09d1acfbbedace9558fa1f2
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-25 23:03:47 +0200

    First try class

--- clase.py
+++ clase.py
@@ -1,36 +1,37 @@
 from bs4 import BeautifulSoup
 import requests, csv, os
-import pandas as pd
+import pandas as pd 
 
-# PRECIO IPHONE X PLATA 64GB
-def iphoneXnegro():
-    url = 'https://www.backmarket.es/iphone-x-64-gb-gris-espacial-libre-segunda-mano/36833.html#?l=4'
-    response = requests.get(url)
-    content = BeautifulSoup(response.content, "html.parser")
+path = r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\CSV"
 
-    eXRnegro = []
-    pXRnegro = []
-    for precio in content.find('div', attrs={"class":"_1OTebAgHAcWddAP7YhPlZr"}):
-        #eXnegro.append("Estado")
-        eXRnegro.append(precio.find('div', attrs={"class":"_3tVEyw2WXK4Ne75JtF4vL4"}).text.strip('\n\xa0€'))
-        #pXnegro.append("Precio")
-        pXRnegro.append(precio.find('div', attrs={"class":"_1KnTWVHWxp0RXYEjGmT44-"}).text.strip('\n\xa0€'))
-        
-    precioXRnegro= dict(zip(eXRnegro,pXRnegro))
-        
-    print(precioXRnegro)
-
-    # Esto borra lo que tenga el CSV y escribe el diccionario
-    # Cambiamos de directorio para guardar el CSV
-    os.chdir(r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\CSV")
-    with open('Xnegro.csv', 'w') as f:
-            for key in precioXRnegro.keys():
-                f.write("%s,%s\n"%(key,precioXRnegro[key]))
+def getPrice():
+    url = {}
+    # TODO las keys serán el nombre del móvil y los values serán las url
     
-    with open('Xnegro.csv', 'r') as file_in:
-        with open('iPhoneXnegro.csv', 'w') as file_out:
-            writer = csv.writer(file_out)
-            for row in csv.reader(file_in):
-                writer.writerow(row[:-1])
+    for k, v in url.items():
+        url = str(v)
+        response = requests.get(url)
+        content = BeautifulSoup(response.content, "html.parser")
+        
+        state = []
+        price = []
+        for i in content.find('div', attrs={"class":"_1OTebAgHAcWddAP7YhPlZr"}):
+            state.append(i.find('div', attrs={"class":"_3tVEyw2WXK4Ne75JtF4vL4"}).text.strip('\n\xa0€'))
+            price.append(i.find('div', attrs={"class":"_1KnTWVHWxp0RXYEjGmT44-"}).text.strip('\n\xa0€'))
+            
+        phonePrice = dict(zip(state,price))
+        
+        print(phonePrice)
+        
+        os.chdir(path)
+        with open('test.csv', 'w') as f:
+            for key in phonePrice.keys():
+                f.write("%s,%s\n"%(key,phonePrice[key]))
                 
-    os.remove('Xnegro.csv')
+        with open('test.csv', 'r') as file_in:
+            with open(k + '.csv', 'w') as file_out:
+                writer = csv.writer(file_out)
+                for row in csv.reader(file_in):
+                    writer.writerow(row[:-1])
+                    
+        os.remove('test.csv')
