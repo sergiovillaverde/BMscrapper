f0b73f818178ccac0f9f6cb46827098951d5a7cd
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-26 15:07:00 +0200

    name changes

---
+++ abandoned.py
@@ -0,0 +1,77 @@
+import os
+from time import sleep
+
+CSVfiles = r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\CSV"
+modelsFile = r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\Models"
+os.chdir(modelsFile)
+
+from Models.iphoneXnegro import iphoneXnegro
+from Models.iphoneXplata import iphoneXplata
+from Models.iphoneXRnegro import iphoneXRnegro
+from Models.iphoneXRrojo import iphoneXRrojo
+from Models.iphoneXRazul import iphoneXRazul
+from Models.iphoneXRcoral import iphoneXRcoral
+from Models.iphoneXRamarillo import iphoneXRamarillo
+from Models.iphoneXRblanco import iphoneXRblanco
+from Models.iphoneXSnegro import iphoneXSnegro
+from Models.iphoneXSplata import iphoneXSplata
+from Models.iphoneXSdorado import iphoneXSdorado
+from deleteCSV import deleteCSV
+from combineCSV import combineCSV
+
+try:
+    print('El programa se ha iniciado.')
+    sleep(2)
+    print('Recopilando precios de iPhone X.')
+    sleep(1)
+    print('iPhone X negro:')
+    iphoneXnegro()
+    sleep(1)
+    print('iPhone X plata:')
+    iphoneXplata()
+    sleep(1)
+
+    print('Recopilando precios de iPhone XR.')
+    sleep(1)
+    print('iPhone XR negro:')
+    iphoneXRnegro()
+    sleep(1)
+    print('iPhone XR rojo:')
+    iphoneXRrojo()
+    sleep(1)
+    print('iPhone XR azul:')
+    iphoneXRazul()
+    sleep(1)
+    print('iPhone XR coral:')
+    iphoneXRcoral()
+    sleep(1)
+    print('iPhone XR amarillo:')
+    iphoneXRamarillo()
+    sleep(1)
+    print('iPhone XR blanco:')
+    iphoneXRblanco()
+    sleep(1)
+
+    print('Recopilando precios de iPhone XS.')
+    sleep(1)
+    print('iPhone XS negro:')
+    iphoneXSnegro()
+    sleep(1)
+    print('iPhone XS plata:')
+    iphoneXSplata()
+    sleep(1)
+    print('iPhone XS dorado:')
+    iphoneXSdorado()
+    sleep(1)
+
+    print('Los archivos CSV están listos, el programa ha terminado.')
+    
+    # Abrimos la carpeta CSV para ahorrar tiempo
+    #os.startfile(CSVfiles) # OJO! Sólo funciona en Windows
+    sleep(7)     
+    
+    #combineCSV()
+    #deleteCSV()
+    
+except:
+    print('Ha surgido un error, el programa se ha detenido.')
--- main.py
+++ main.py
@@ -1,77 +1,79 @@
-import os
-from time import sleep
+from bs4 import BeautifulSoup
+import requests, csv, os
+import pandas as pd 
+import pyinputplus as pyip 
+from pathlib import Path
 
-CSVfiles = r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\CSV"
-modelsFile = r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\Models"
-os.chdir(modelsFile)
+path = r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\CSV"
 
-from Models.iphoneXnegro import iphoneXnegro
-from Models.iphoneXplata import iphoneXplata
-from Models.iphoneXRnegro import iphoneXRnegro
-from Models.iphoneXRrojo import iphoneXRrojo
-from Models.iphoneXRazul import iphoneXRazul
-from Models.iphoneXRcoral import iphoneXRcoral
-from Models.iphoneXRamarillo import iphoneXRamarillo
-from Models.iphoneXRblanco import iphoneXRblanco
-from Models.iphoneXSnegro import iphoneXSnegro
-from Models.iphoneXSplata import iphoneXSplata
-from Models.iphoneXSdorado import iphoneXSdorado
-from deleteCSV import deleteCSV
-from combineCSV import combineCSV
-
-try:
-    print('El programa se ha iniciado.')
-    sleep(2)
-    print('Recopilando precios de iPhone X.')
-    sleep(1)
-    print('iPhone X negro:')
-    iphoneXnegro()
-    sleep(1)
-    print('iPhone X plata:')
-    iphoneXplata()
-    sleep(1)
-
-    print('Recopilando precios de iPhone XR.')
-    sleep(1)
-    print('iPhone XR negro:')
-    iphoneXRnegro()
-    sleep(1)
-    print('iPhone XR rojo:')
-    iphoneXRrojo()
-    sleep(1)
-    print('iPhone XR azul:')
-    iphoneXRazul()
-    sleep(1)
-    print('iPhone XR coral:')
-    iphoneXRcoral()
-    sleep(1)
-    print('iPhone XR amarillo:')
-    iphoneXRamarillo()
-    sleep(1)
-    print('iPhone XR blanco:')
-    iphoneXRblanco()
-    sleep(1)
-
-    print('Recopilando precios de iPhone XS.')
-    sleep(1)
-    print('iPhone XS negro:')
-    iphoneXSnegro()
-    sleep(1)
-    print('iPhone XS plata:')
-    iphoneXSplata()
-    sleep(1)
-    print('iPhone XS dorado:')
-    iphoneXSdorado()
-    sleep(1)
-
-    print('Los archivos CSV están listos, el programa ha terminado.')
+def getPrice():
+    url = {}
+    loadBool = False
+    loadNum = 0
     
-    # Abrimos la carpeta CSV para ahorrar tiempo
-    #os.startfile(CSVfiles) # OJO! Sólo funciona en Windows
-    sleep(7)     
+    while True:
+        CSVexist = os.path.isfile('./phonesURL.csv')
+        if CSVexist == True:
+            # Ask the user to load the CSV with the info of the devices to create the dict
+            if loadNum < 1:
+                load = pyip.inputChoice(['yes','no'], prompt='Do you want to load the previous consult?\n')
+                if load == 'yes':
+                    with open('phonesURL.csv', 'r') as infile:
+                        reader = csv.reader(infile)
+                        with open('csvtodict.csv', 'w') as outfile:
+                            writer = csv.writer(outfile)
+                            url = {rows[0]:rows[1] for rows in reader}
+                
+                    os.remove('csvtodict.csv')
+                    loadBool = True
+                    break
+                elif load == 'no':
+                    loadNum = 1
+        
+        # Ask the user for the name and URL of the device
+        name = input('Which is the name of the phone?\n')
+        phoneURL = input('Which is the URL of the phone?\n')
+        #phoneName = name + phoneURL
+        #url = dict(phoneName.split())
+        url.update({name:phoneURL})
+        more = pyip.inputChoice(['yes','no'], prompt='Do you want to track more phones?\n')
+        if more == 'no':
+            break
     
-    #combineCSV()
-    #deleteCSV()
+    # Ask the user to save the info of the chosen devices
+    if loadBool == False:
+        save = pyip.inputChoice(['yes','no'], prompt='Do you want to save the URL for the next time?\n')
+        if save == 'yes':
+            with open('phonesURL.csv', 'w') as f:
+                for key in url.keys():
+                    f.write("%s,%s\n"%(key,url[key]))
     
-except:
-    print('Ha surgido un error, el programa se ha detenido.')
+    for k, v in url.items():
+        link = str(v)
+        response = requests.get(link)
+        content = BeautifulSoup(response.content, "html.parser")
+        
+        state = []
+        price = []
+        for i in content.find('div', attrs={"class":"_1OTebAgHAcWddAP7YhPlZr"}):
+            state.append(i.find('div', attrs={"class":"_3tVEyw2WXK4Ne75JtF4vL4"}).text.strip('\n\xa0€'))
+            price.append(i.find('div', attrs={"class":"_1KnTWVHWxp0RXYEjGmT44-"}).text.strip('\n\xa0€'))
+            
+        phonePrice = dict(zip(state,price))
+        
+        os.chdir(path)
+        with open('test.csv', 'w') as f:
+            for key in phonePrice.keys():
+                f.write("%s,%s\n"%(key,phonePrice[key]))
+                
+        with open('test.csv', 'r') as file_in:
+            with open(k + '.csv', 'w') as file_out:
+                writer = csv.writer(file_out)
+                for row in csv.reader(file_in):
+                    writer.writerow(row[:-1])
+                    
+        os.remove('test.csv')
+        
+        print(k + ' price was tracked.')
+        
+getPrice()
