b31932b73ca0553c4c0bd315baf84ce0a894fbdc
sergiovillaverde <svillaverdebusto@gmail.com>
2020-08-28 16:19:48 +0200

    functions created

--- mainFunctions.py
+++ mainFunctions.py
@@ -1,8 +1,13 @@
-import os, csv
-import pyinputplus as pyip 
+from bs4 import BeautifulSoup
+import os, csv, requests
+import pyinputplus as pyip
+import pandas as pd 
+#from pathlib import Path 
 
 # Variables globales
 url = dict
+# Path va a dar problemas en otros PCs
+path = r"C:\Users\svill\Documents\Programación\Proyectos cortos Python\Backmarket scrapper\BMscrapper\CSV"
 
 # Crear loadNum para sacar del loop esta función si fuera necesario
 def load():
@@ -33,7 +38,7 @@ def manualInput():
         phoneURL = input('Which is the URL of the device?\n')
         url.update({name:phoneURL})
         more = pyip.inputChoice(['yes','no'], prompt='Do you want to track more devices?\n')    
-        if more == no:
+        if more == 'no':
             break
         
     save = pyip.inputChoice(['yes','no'], prompt='Do you want to save your selection?\n')
@@ -43,6 +48,33 @@ def manualInput():
                 f.write("%s,%s\n"%(key,url[key]))
 
 def getInfo():
+    # Global variables
+    global url, path
+    
     # función del for para hacer el scrap y crear los CSV
-    pass
-
+    for k, v in url.items():
+        link = str(v)
+        response = requests.get(link)
+        content = BeautifulSoup(response.content,"html.parser")
+        
+        quality = []
+        price = []
+        for i in content.find('div', attrs={"class":"_1OTebAgHAcWddAP7YhPlZr"}):
+            quality.append(i.find('div', attrs={"class":"_3tVEyw2WXK4Ne75JtF4vL4"}).text.strip('\n\xa0€'))
+            price.append(i.find('div', attrs={"class":"_1KnTWVHWxp0RXYEjGmT44-"}).text.strip('\n\xa0€'))
+            
+        phonePrice = dict(zip(quality,price))
+        
+        os.chdir(path)
+        with open('test.csv','r') as f:
+            for key in phonePrice.keys():
+                f.write("%s,%s\n"%(key,phonePrice[key]))
+                
+        with open('test.csv','r') as file_in:
+            with open(k + '.csv','w') as file_out:
+                writer = csv.writer(file_out)
+                for row in csv.reader(file_in):
+                    writer.writerow(row[:-1])
+                    
+        os.remove('test.csv')
+        print(k + ' price was tracked.')
